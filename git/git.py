c197478452b1019cebdad100aba4646ed6d1a0b1
Hilal Jaffan <hj@jaffan.io>
2019-11-09 19:14:30 -0800

    Adding new function to push branch

--- git/git.py
+++ git/git.py
@@ -5,21 +5,35 @@ import os
 
 def CloneRepo(repo, username, password):
     try:
-        exec("git clone https://{}:{}@{} repo-dir".format(username,password,repo))
+        os.system("git clone https://{}:{}@{} repo-dir".format(username,password,repo))
     except Exception as e:
         os.error(e)
         exit(100) # 100 Error means failed to clone repo
 
 def CreateBranch(branch_name):
     try:
-        exec("git checkout -B {}".format(branch_name))
+        os.system("cd repo-dir && git checkout -B {}".format(branch_name))
     except Exception as e:
         os.error(e)
         exit(200)
 
 def CommitChanges(branch_name):
     try:
-        exec("git commit -am 'Updating Instance type via automation'")
+        os.system("cd repo-dir && git commit -am 'Updating Instance type via automation'")
     except Exception as e:
         os.error(e)
         exec(300)
+
+def PushBranch(origin, branch_name):
+    try:
+        os.system("cd repo-dir && git push -u {} {}".format(origin, branch_name))
+    except Exception as e:
+        os.error(e)
+        exec(300)    
+
+def CleanRepo():
+    try:
+        os.system("rm -rf repo-dir/")
+    except Exception as e:
+        os.error(e)
+        exec(400)
