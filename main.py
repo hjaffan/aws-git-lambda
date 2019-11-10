052aa3857f0358f94e5a21df570d2ca329bf79f1
Hilal Jaffan <hj@jaffan.io>
2019-11-09 19:48:14 -0800

    Adding the workflow for the creation of the branch

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import git
+from git import git
+from files import terraform
+import json
+import boto3
 import variables
 
+
+def parse_webhook(event, context):
+  
+  """
+  Get payload from API Gateway
+  """
+  tfvars = {}
+  tfvars['environment'] = event['environment']
+  tfvars['cpuCount'] = event['cpuCount']
+  tfvars['memoryGB'] = event['memoryGB']
+
+  git.CloneRepo(variables.GIT_REPO, variables.GIT_USERNAME, variables.GIT_PASSWORD)
+  terraform.AppendTFVarsFile(tfvars)
+  git.CreateBranch(event['snowRequestId'])
+  git.CommitChanges(event['snowRequestId'])
+  git.PushBranch("origin", event['snowRequestId'])
+  git.CleanRepo()
