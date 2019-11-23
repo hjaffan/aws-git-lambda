e84a73d76148ff2c2233dada35a1b750da49efae
Hilal Jaffan <hj@jaffan.io>
2019-11-23 14:51:32 -0800

    Adding terraform

---
+++ main.tf
@@ -0,0 +1,53 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+provider "archive" {}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "."
+  output_path = "parse_webhook.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "parse_webhook"
+
+  filename         = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+
+  role    = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "main.parse_webhook"
+  runtime = "python3.7"
+
+  environment {
+    variables = {
+      GIT_REPO = "",
+      FULL_NAME = "",
+      GIT_EMAIL = "",
+      GIT_USERNAME = "",
+      GIT_PASSWORD = "",
+      SKIP_SSL = "True"
+    }
+  }
+}
